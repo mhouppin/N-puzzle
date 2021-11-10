b68b7aef20efa17e45eca8ab9419ec4e5318b5d6
Morgan Houppin <morganhouppin@gmail.com>
2021-11-10 10:17:14 +0100

    Updated submodule, added structure NPuzzle

--- include/npuzzle.h
+++ include/npuzzle.h
@@ -1,6 +1,15 @@
 #ifndef NPUZZLE_H
 # define NPUZZLE_H
 
+# include <stddef.h>
+# include <stdint.h>
 
+// Structure for holding a single n-puzzle.
+typedef struct NPuzzle_
+{
+    size_t size;
+    uint16_t *board;
+}
+NPuzzle;
 
 #endif
